06317b2fd253995576671a61f8b42dc816595fbd
Emmanuel <ogoma.emmanuel@gmail.com>
2018-01-04 17:04:56 +0300

    reosleved missing glaccount for loans/products repayments

--- EsoftMobileApi/Services/TellerManager.cs
+++ EsoftMobileApi/Services/TellerManager.cs
@@ -147,7 +147,7 @@ namespace EsoftMobileApi.Services
                         trdescpt = String.Format("Loan Repayment Mobile App: {0}", referenceNo);
                         var loanProduct = loanCodes.FirstOrDefault(x => x.LoanCode == repayment.ProductCode);
                         loanProductsMgr.Distribute_LoanRepayment(repayment, customerBalances, loanProduct, true);
-
+                        glaccount_cr = loanProduct.PrincipalAccount;
                         loanProductsMgr.GenerateLoanRepaymentStatements(transactionsEngine, translist, m_transactionid, repayment, loanProduct, trdatenow, trdescpt, docid, referenceNo, 0, 0,
                             income_branch, tellerAccount, "1", "", false, db);
 
@@ -186,6 +186,9 @@ namespace EsoftMobileApi.Services
             if (result == "Transactions Was Updated Successfully ")
             {
                 transactionPosted = true;
+            }
+            else {
+                _validationDictionary.AddError("", result);
             }
 
             return transactionPosted;
